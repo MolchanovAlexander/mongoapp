41ecda48576b17b26208e00a69e39bb392904de3
MolchanovAlexander <91253664+MolchanovAlexander@users.noreply.github.com>
2023-10-15 20:33:43 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import Navbar from '@/components/Navbar'
+import Notification from '@/components/Notification'
+import './globals.css'
+import type { Metadata } from 'next'
+//import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer'
+import AuthProvider from '@/components/AuthProvider'
+import QueryProvider from '@/components/QueryProvider'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+
+//const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Newbie restaurant',
+  description: 'Food for newbies',
+}
+//export const dynamic = 'force-dynamic'
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+     <body className=""> 
+        <AuthProvider>
+          <QueryProvider>
+            <Notification />
+            <Navbar />
+            {children}
+            <Footer />
+            <ToastContainer position='bottom-right' theme='dark' autoClose={2000}/>
+          </QueryProvider>
+        </AuthProvider>
+      </body>
+    </html>
+  )
+}
